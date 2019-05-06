c1b6d9644ff04a1de51302ca011cf644f9c74870
Lukáš Kurčík <lukas.kurcik@gmail.com>
2019-05-06 20:42:00 +0200

    GoogleImage getter

---
+++ RClone Anime/Image/GoogleImage.cs
@@ -0,0 +1,28 @@
+using System.Net;
+using System.Security.Policy;
+using HtmlAgilityPack;
+
+namespace RClone_Anime.Image
+{
+    public class GoogleImage
+    {
+
+        private const string ImageSearchUrl = "https://www.google.com/search?q={0}&tbm=isch";
+        
+        public static string getFirst(string phrase)
+        {
+            var url = getImageSearchUrl(phrase);
+            
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+            
+            return doc.DocumentNode.SelectSingleNode("//table[@class='images_table']//img").Attributes["src"].Value;
+        }
+
+        private static string getImageSearchUrl(string phrase)
+        {
+            var encodedPhrase = WebUtility.UrlEncode(phrase);
+            return string.Format(ImageSearchUrl, encodedPhrase);
+        }
+    }
+}
